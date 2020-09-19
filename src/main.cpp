77752a86e2739e0c924f758d3a37ab7b3efb9059
fwsmit <fw.smit01@gmail.com>
2020-09-19 18:13:29 +0200

    initial code

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <armadillo>
 #include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
+#include <cmath>
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "");
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML kaleidoscope");
 	
 	while (window.isOpen())
 	{
@@ -13,6 +15,7 @@ int main()
 		while (window.pollEvent(event))
 		{
 			switch (event.type){
+
 				case sf::Event::Closed:
 					window.close();
 					break;
@@ -21,7 +24,28 @@ int main()
 			}
 		}
 
-		window.clear();
+		// window.clear();
+		if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			for(size_t i = 0; i < 6; i++)
+			{
+				float rotation = 1/3 * M_PI * i;
+				sf::CircleShape s(10);
+				s.setFillColor(sf::Color::White);
+				s.setOrigin(s.getRadius(), s.getRadius());
+				auto temp = sf::Vector2f(sf::Mouse::getPosition(window));
+				arma::vec2 pos = {temp.x, temp.y};
+				arma::mat22 rot = { {std::cos(rotation), -std::sin(rotation)}, 
+					{std::sin(rotation), std::cos(rotation)}};
+				auto newPos = rot*pos;
+
+				std::cout << newPos << std::endl;
+				// auto converted = sf::Vector2f(newPos[0], newPos[1]);
+				// s.setPosition(converted);
+				window.draw(s);
+			}
+		}
+		
 		window.display();
 	}
 }
